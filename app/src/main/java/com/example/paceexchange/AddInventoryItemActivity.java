853926391064c3192d49897138541ec563fdd910
“Keith” <ina7cio@gmail.com>
2019-05-06 21:09:18 -0400

    add item updates

--- app/src/main/java/com/example/paceexchange/AddInventoryItemActivity.java
+++ app/src/main/java/com/example/paceexchange/AddInventoryItemActivity.java
@@ -7,18 +7,63 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class AddInventoryItemActivity extends AppCompatActivity {
 
+    private EditText mNewItemInput;
+    private Button mSubmitItemButton;
+    private RadioGroup mConditionRadioGroup;
+    private RadioButton mSelectedRadioButton;
     private Spinner mSpinner;
     private ArrayAdapter<CharSequence> mItemAdapter;
+    private String mNewItemName, mNewitemConditionm, mNewItemCategory;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_inventory);
+
         mSpinner = findViewById(R.id.spinner);
+        mNewItemInput = findViewById(R.id.itemNameInput);
+        mConditionRadioGroup = findViewById(R.id.productConditionButtons);
+        mSubmitItemButton = findViewById(R.id.submitNewItemButton);
+        setOnItemMenuClickListener();
+        setOnButtonClickListener();
+
+
+    }
+
+    public void getNewItemData() {
+
+        int selectedConditionButtonID = mConditionRadioGroup.getCheckedRadioButtonId();
+        mSelectedRadioButton = findViewById(selectedConditionButtonID);
+
+        if (mNewItemInput.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), R.string.error_new_item_name, Toast.LENGTH_LONG).show();
+        } else if (mSelectedRadioButton == null) {
+            Toast.makeText(getApplicationContext(), R.string.error_new_item_radio_button, Toast.LENGTH_LONG).show();
+        } else {
+            mNewItemName = mNewItemInput.getText().toString().trim();
+        }
+    }
+
+    public void setOnButtonClickListener() {
+
+        mSubmitItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getNewItemData();
+            }
+        });
+    }
+
+    public void setOnItemMenuClickListener() {
 
         mItemAdapter = ArrayAdapter.createFromResource(this, R.array.item_array, android.R.layout.simple_spinner_item);
         mItemAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -27,7 +72,7 @@ public class AddInventoryItemActivity extends AppCompatActivity {
         mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Log.v("item", (String) parent.getItemAtPosition(position));
+                mNewItemCategory = (String) parent.getItemAtPosition(position);
             }
 
             @Override
